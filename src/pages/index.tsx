b0b3c215fd02ced4d199670ade9f9b90a37a3726
Mathieu KHALEM <mathieu.khalem@gmail.com>
2021-12-23 19:19:49 +0100

    Blank page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,29 +4,31 @@ import React, { FC } from 'react'
 import Layout from '../layout/MainLayout'
 
 const IndexPage: FC = () => {
-  return (
-    <Layout>
-      <Stack direction="column" spacing={2} alignItems="center">
-        <Typography variant="h3">Je veux...</Typography>
-        <Grid container>
-          <Grid item xs={12} md={6} textAlign="center">
-            <Link href="/offres/site-web">
-              <Button variant="contained" color="secondary">
-                Un site web
-              </Button>
-            </Link>
-          </Grid>
-          <Grid item xs={12} md={6} textAlign="center">
-            <Link href="/offres/logiciel">
-              <Button variant="contained" color="secondary">
-                Un logiciel sur-mesure
-              </Button>
-            </Link>
-          </Grid>
-        </Grid>
-      </Stack>
-    </Layout>
-  )
+  return <div></div>
+}
+
+{
+  /* <Layout>
+<Stack direction="column" spacing={2} alignItems="center">
+  <Typography variant="h3">Je veux...</Typography>
+  <Grid container>
+    <Grid item xs={12} md={6} textAlign="center">
+      <Link href="/offres/site-web">
+        <Button variant="contained" color="secondary">
+          Un site web
+        </Button>
+      </Link>
+    </Grid>
+    <Grid item xs={12} md={6} textAlign="center">
+      <Link href="/offres/logiciel">
+        <Button variant="contained" color="secondary">
+          Un logiciel sur-mesure
+        </Button>
+      </Link>
+    </Grid>
+  </Grid>
+</Stack>
+</Layout> */
 }
 
 export default IndexPage

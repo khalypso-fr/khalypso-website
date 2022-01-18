e210fce0574886b2ab644db824ada8ca0c9a7b51
Mathieu Khalem <mathieu.khalem@gmail.com>
2022-01-18 14:42:24 +0100

    Add colors to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          main: '#102f41',
+          light: '#3c586c',
+          dark: '#00051b',
+          contrastText: '#ffffff',
+        },
+        secondary: {
+          main: '#ffc107',
+          light: '#fff350',
+          dark: '#c79100',
+          contrastText: '#000000',
+        },
+      },
+    },
   },
   plugins: [],
 }

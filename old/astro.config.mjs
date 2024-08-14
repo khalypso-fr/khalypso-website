2ec3f2ac90e697301c78ad9a693fa31e1c26fc3e
Mathieu KHALEM <mathieu@khalypso.fr>
2024-08-14 14:35:11 +0200

    Add blog prefix

--- old/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-import icon from "astro-icon";
-
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), icon()]
-});

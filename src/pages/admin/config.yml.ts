ef2137deae506001ff464858721410d26b1ea79b
Mathieu KHALEM <mathieu@khalypso.fr>
2024-08-19 09:57:39 +0200

    Move CMS config.yml in /admin path

--- src/pages/admin/config.yml.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'node:fs/promises';
-import path from 'node:path';
-
-export const GET = async () => {
-  const filePath = path.join(import.meta.dirname, '..', '..', 'cms_config.yml');
-  const buffer = await fs.readFile(filePath);
-  return new Response(buffer);
-};
